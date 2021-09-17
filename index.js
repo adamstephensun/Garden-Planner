3cea221a9c7ddb5d9d71c2a8c2d18914cc58fac1
adamstephensun <adamski.david1@gmail.com>
2021-09-17 18:24:22 +0100

    Added help menu

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let canInteract = new Boolean;
 let helpActive = new Boolean;
 
 let exporter, link, confirmExport, confirmExportTimer, filenameInput, infoBox;
-let helpButton, helpBox; 
+let helpButton, helpBox, helpBack, helpForward, helpPage, helpH2, helpP; 
 
 const collisionObjects = [];
 const nodes = [];
@@ -165,6 +165,7 @@ function init() {
             snapToGrid: true,
             finalPlane: function() {    //Finalise the plane. Removes the folder so plane can't be changed again
                 gui.removeFolder(planeFolder);
+                addRestOfGUI();
                 console.log("Plane finalised");
             }
         },
@@ -429,80 +430,7 @@ function init() {
     planeFolder.add(world.plane, "finalPlane").name("Finalise plane");  //Button to finalise plane. Removes plane folder
     planeFolder.open();
 
-    const lightFolder = gui.addFolder("Lighting");
-    lightFolder.add(world.lights, "timeScale", 1,10, 1).name("Orbit speed");
-    lightFolder.add(world.lights, "sunCycleActive").name("Sun cycle");
-    lightFolder.add(world.lights, "flatLighting").name("Flat lighting").onChange(()=>{
-        sunLight.visible = !world.lights.flatLighting;
-        moonLight.visible = !world.lights.flatLighting;
-        sunSphere.visible = !world.lights.flatLighting;
-        moonSphere.visible = !world.lights.flatLighting;
 
-        if(world.lights.flatLighting){
-            hemiLight.intensity = 0.8;
-            scene.background = skyColour;
-        }
-        else hemiLight.intensity = 0.3;
-    } );
-
-    const areaFolder = gui.addFolder("Area");       //Area folder added
-    areaFolder.add(world.area, "type").options(areaTypes).name("Terrain type");  //Add area type dropdown selector
-    areaFolder.add(world.area, "createNew").name("New area (Q)");       //Add new area button
-    areaFolder.add(world.area, "continueArea").name("Continue Area (W)");
-    areaFolder.add(world.area,"finishArea").name("Finish area (E)");    //add finish area button
-    areaFolder.add(world.area, "clearAreas").name("Clear areas (R)");
-
-    const objectFolder = gui.addFolder("Objects");              //Add the objects folder
-    objectFolder.add(world.objects, "place").name("Place (A)");     //Add place, remove, move buttons
-    objectFolder.add(world.objects, "remove").name("Remove (S)");
-    objectFolder.add(world.objects, "move").name("Move (D)");
-
-    const treeFolder = objectFolder.addFolder("Trees");         //Add tree folder to the objects folder
-    treeFolder.add(world.objects.trees, "tree1").name("Tree 1");
-    treeFolder.add(world.objects.trees, "tree2").name("Tree 2");
-
-    const plantFolder = objectFolder.addFolder("Plants");
-    plantFolder.add(world.objects.plants, "tomato").name("Tomato");
-    plantFolder.add(world.objects.plants, "strawberry").name("Strawberry");
-    plantFolder.add(world.objects.plants, "cucumber").name("Cucumber");
-    plantFolder.add(world.objects.plants, "lettuce").name("Lettuce");
-    plantFolder.add(world.objects.plants, "carrot").name("Carrot");
-
-    const flowerFolder = objectFolder.addFolder("Flowers");
-    flowerFolder.add(world.objects.flowers, "pot").name("Flower pot");
-    flowerFolder.add(world.objects.flowers, "redFlower").name("Red flower");
-    flowerFolder.add(world.objects.flowers, "whiteFlower").name("White flower");
-    flowerFolder.add(world.objects.flowers, "sunflower").name("Sunflower");
-
-    const bushFolder = objectFolder.addFolder("Bushes");
-    bushFolder.add(world.objects.bushes, "bush1").name("Bush 1");
-    bushFolder.add(world.objects.bushes, "bush2").name("Bush 2");
-    bushFolder.add(world.objects.bushes, "bush3").name("Bush 3");
-
-    const furnitureFolder = objectFolder.addFolder("Furniture");
-    furnitureFolder.add(world.objects.furniture.benches, "bench1").name("Bench 1");
-    furnitureFolder.add(world.objects.furniture.chairs, "chair1").name("Chair 1");
-    furnitureFolder.add(world.objects.furniture.tables, "table1").name("Table 1");
-
-    const bedsFolder = objectFolder.addFolder("Beds");
-    bedsFolder.add(world.objects.beds, "squareRaised").name("Square raised");
-    bedsFolder.add(world.objects.beds, "rectRaised").name("Rectangle raised");
-    bedsFolder.add(world.objects.beds, "triangleRaised").name("Triangle raised");
-
-    const wallFolder = objectFolder.addFolder("Walls");
-    wallFolder.add(world.objects.walls, "fenceLow").name("Low fence");
-    wallFolder.add(world.objects.walls, "fenceHigh").name("High fence");
-    wallFolder.add(world.objects.walls, "brickLow").name("Low brick");
-    wallFolder.add(world.objects.walls, "brickHigh").name("High brick");
-    wallFolder.add(world.objects.walls, "stoneLow").name("Low stone");
-    wallFolder.add(world.objects.walls, "stoneHigh").name("High stone");
-
-    const leisureFolder = objectFolder.addFolder("Leisure");
-    leisureFolder.add(world.objects.leisure, "bbq").name("BBQ");
-    leisureFolder.add(world.objects.leisure, "firepit").name("Firepit");
-    leisureFolder.add(world.objects.leisure, "swing").name("Swing");
-    leisureFolder.add(world.objects.leisure, "goal").name("Goal");
-    leisureFolder.add(world.objects.leisure, "sandbox").name("Sandbox");
 
     //#endregion GUI folders
 
@@ -608,19 +536,32 @@ function init() {
     link = document.createElement('a'); //Code for gltf exporter
     link.style.display = "none";
     document.body.appendChild(link);
-    
 
+    //////Export scene//////
     document.getElementById('export-scene').addEventListener('click', function(){exportScene();})
     confirmExport = document.getElementById("export-confirm");
     confirmExport.style.visibility = "hidden";
-    
     filenameInput = document.getElementById('filename');
-    infoBox = document.getElementById("info-box");
+    
+    /////help/////
     helpButton = document.getElementById("help-button").addEventListener('click', function(){toggleHelp();});
     helpBox = document.getElementById("help");
-
+    helpBack = document.getElementById("help-back").addEventListener('click', function(){
+        helpPage--;
+        updateHelp();
+    });
+    helpForward = document.getElementById("help-forward").addEventListener('click', function(){
+        helpPage++;
+        updateHelp();
+    });
+    helpH2 = document.getElementById("help-h2");
+    helpP = document.getElementById("help-p");
+    helpPage = 0;
+    updateHelp();
     toggleHelp();
     
+    /////info box (top left)//////
+    infoBox = document.getElementById("info-box");
     infoBox.addEventListener('mouseenter', function(){ canInteract = false; })
     infoBox.addEventListener('mouseleave', function(){ canInteract = true; })
 
@@ -628,6 +569,113 @@ function init() {
     gui.domElement.addEventListener('mouseleave', function(){ canInteract = true; })
 
     //#endregion HTML
+
+function addRestOfGUI(){
+    const lightFolder = gui.addFolder("Lighting");
+    lightFolder.add(world.lights, "timeScale", 1,10, 1).name("Orbit speed");
+    lightFolder.add(world.lights, "sunCycleActive").name("Sun cycle");
+    lightFolder.add(world.lights, "flatLighting").name("Flat lighting").onChange(()=>{
+        sunLight.visible = !world.lights.flatLighting;
+        moonLight.visible = !world.lights.flatLighting;
+        sunSphere.visible = !world.lights.flatLighting;
+        moonSphere.visible = !world.lights.flatLighting;
+
+        if(world.lights.flatLighting){
+            hemiLight.intensity = 0.8;
+            scene.background = skyColour;
+        }
+        else hemiLight.intensity = 0.3;
+    } );
+
+    const areaFolder = gui.addFolder("Area");       //Area folder added
+    areaFolder.add(world.area, "type").options(areaTypes).name("Terrain type");  //Add area type dropdown selector
+    areaFolder.add(world.area, "createNew").name("New area (Q)");       //Add new area button
+    areaFolder.add(world.area, "continueArea").name("Continue Area (W)");
+    areaFolder.add(world.area,"finishArea").name("Finish area (E)");    //add finish area button
+    areaFolder.add(world.area, "clearAreas").name("Clear areas (R)");
+
+    const objectFolder = gui.addFolder("Objects");              //Add the objects folder
+    objectFolder.add(world.objects, "place").name("Place (A)");     //Add place, remove, move buttons
+    objectFolder.add(world.objects, "remove").name("Remove (S)");
+    objectFolder.add(world.objects, "move").name("Move (D)");
+
+    const treeFolder = objectFolder.addFolder("Trees");         //Add tree folder to the objects folder
+    treeFolder.add(world.objects.trees, "tree1").name("Tree 1");
+    treeFolder.add(world.objects.trees, "tree2").name("Tree 2");
+
+    const plantFolder = objectFolder.addFolder("Plants");
+    plantFolder.add(world.objects.plants, "tomato").name("Tomato");
+    plantFolder.add(world.objects.plants, "strawberry").name("Strawberry");
+    plantFolder.add(world.objects.plants, "cucumber").name("Cucumber");
+    plantFolder.add(world.objects.plants, "lettuce").name("Lettuce");
+    plantFolder.add(world.objects.plants, "carrot").name("Carrot");
+
+    const flowerFolder = objectFolder.addFolder("Flowers");
+    flowerFolder.add(world.objects.flowers, "pot").name("Flower pot");
+    flowerFolder.add(world.objects.flowers, "redFlower").name("Red flower");
+    flowerFolder.add(world.objects.flowers, "whiteFlower").name("White flower");
+    flowerFolder.add(world.objects.flowers, "sunflower").name("Sunflower");
+
+    const bushFolder = objectFolder.addFolder("Bushes");
+    bushFolder.add(world.objects.bushes, "bush1").name("Bush 1");
+    bushFolder.add(world.objects.bushes, "bush2").name("Bush 2");
+    bushFolder.add(world.objects.bushes, "bush3").name("Bush 3");
+
+    const furnitureFolder = objectFolder.addFolder("Furniture");
+    furnitureFolder.add(world.objects.furniture.benches, "bench1").name("Bench 1");
+    furnitureFolder.add(world.objects.furniture.chairs, "chair1").name("Chair 1");
+    furnitureFolder.add(world.objects.furniture.tables, "table1").name("Table 1");
+
+    const bedsFolder = objectFolder.addFolder("Beds");
+    bedsFolder.add(world.objects.beds, "squareRaised").name("Square raised");
+    bedsFolder.add(world.objects.beds, "rectRaised").name("Rectangle raised");
+    bedsFolder.add(world.objects.beds, "triangleRaised").name("Triangle raised");
+
+    const wallFolder = objectFolder.addFolder("Walls");
+    wallFolder.add(world.objects.walls, "fenceLow").name("Low fence");
+    wallFolder.add(world.objects.walls, "fenceHigh").name("High fence");
+    wallFolder.add(world.objects.walls, "brickLow").name("Low brick");
+    wallFolder.add(world.objects.walls, "brickHigh").name("High brick");
+    wallFolder.add(world.objects.walls, "stoneLow").name("Low stone");
+    wallFolder.add(world.objects.walls, "stoneHigh").name("High stone");
+
+    const leisureFolder = objectFolder.addFolder("Leisure");
+    leisureFolder.add(world.objects.leisure, "bbq").name("BBQ");
+    leisureFolder.add(world.objects.leisure, "firepit").name("Firepit");
+    leisureFolder.add(world.objects.leisure, "swing").name("Swing");
+    leisureFolder.add(world.objects.leisure, "goal").name("Goal");
+    leisureFolder.add(world.objects.leisure, "sandbox").name("Sandbox");
+}
+
+function updateHelp(){
+    if(helpPage <0) helpPage = 4;
+    if(helpPage >4) helpPage = 0;
+    console.log("Changing to help page: "+helpPage);
+
+    switch(helpPage)
+    {
+        case 0:
+            helpH2.innerHTML = "1: Plane";
+            helpP.innerHTML = "Welcome to garden planner! Use this tool to plan your dream garden!  <br> Use the menu on the top right of the screen to make changes. <br> Start by altering the plane by changing the size, terrain type, and grid snapping. Click finalise to finish editing the plane. <br> Press the help button to close this menu. <br><br>";
+            break;
+        case 1:
+            helpH2.innerHTML = "2: Lighting";
+            helpP.innerHTML = "Use the lighting panel to adjust the sun/moon orbit speed or disable it. <br> You can also enable flat lighting for constant lighting with no shadows. <br> <br> <br>";
+            break;
+        case 2:
+            helpH2.innerHTML = "3: Area";
+            helpP.innerHTML = "This panel is used to create areas of different types on top of the main plane. <br> You can choose the type of area you want to create, then enter area creation mode by clicking the button or pressing the Q key. <br> Click on the plane to create the outer points and the area will be created on the forth point. You can also create the area with less points using the E key. <br> <br> <br>";
+            break;
+        case 3:
+            helpH2.innerHTML = "4: Objects";
+            helpP.innerHTML = "The objects panel holds all the placable props in folders, such as trees, plants, and furniture.<br> There are also tools to be selected for placing objects (A key), removing objects (S key), and moving objects (D key). <br> <br> <br>";
+            break;
+        case 4:
+            helpH2.innerHTML = "5: Exporting";
+            helpP.innerHTML = "The 'export scene' button in the top-left allows you to export the scene as a .glTF model. This includes the plane and all the objects you placed on it. <br> You can enter a file name in the field below to rename the file. <br> <br>";
+            break;
+    }
+}
 
 function updateCurrentObjectPath(){
     switch(currentObject)
@@ -1344,7 +1392,6 @@ function loadObjectRollover()
         scene.add(objectRolloverMesh);
         objectRolloverMesh.visible = false;
     });
-    console.log("obj: "+objectRolloverMesh);
 }
 
 function loadFlagRollover(){
@@ -1440,6 +1487,12 @@ function toggleHelp(){
 
     helpActive = !helpActive;
 
-    if(helpActive) helpBox.style.visibility = "visible";
-    else helpBox.style.visibility = "hidden";
+    if(helpActive){
+        helpBox.style.visibility = "visible";
+        gui.hide();
+    }
+    else {
+        helpBox.style.visibility = "hidden";
+        gui.show();
+    }
 }
